5cb0d256cf181d0da1132bcef7be890f216ba1a9
Jesse <wjesse12@gmail.com>
2024-03-27 00:17:10 -0400

    Added in parking meter image, also updated the advanced marker to be custom

--- src/views/App.js
+++ src/views/App.js
@@ -2,6 +2,7 @@ import '../css/App.css';
 import NavbarComponent from '../components/Navbar';
 import getParkingMeterMapPoints from '../utils/maps/getParkingMeterPoints';
 import React, {useState, useEffect} from 'react';
+import logo from '../assets/img/parkingmeter.png';
 import {
   APIProvider,
   Map,
@@ -25,20 +26,17 @@ const App = () => {
   return (
     <div style={{width: "100vw", height: "100vh"}}>
       <NavbarComponent/>
-      <APIProvider apiKey={'API KEY'}>
-        <Map style={{width: "100%", height: "100%"}} defaultCenter={{lat: 42.1292, lng: -80.0851}}>
+      <APIProvider apiKey={require('../config.json').MapsKey}>
+        <Map style={{width: "100%", height: "100%"}} defaultCenter={{lat: 42.1292, lng: -80.0851}} defaultZoom={14} mapId={"erie-parking-meters"}>
           {mapPoints && mapPoints.map((point, index) => (
             <AdvancedMarker
               key={index}
               position={{lat: point.location.latitude, lng: point.location.longitude}}
-              onClick={() => setSelectedPoint(point)}
-            />
+              onClick={() => setSelectedPoint(point)}>
+              <p style={{textAlign: "center", fontWeight: "bold"}}>{`Meter #${point.number}`}</p>
+              <img style={{height: "50px", width: "50px"}} src={logo}/>
+            </AdvancedMarker>
           ))}
-          {selectedPoint && (
-            <InfoWindow anchor={selectedPoint} onCloseClick={() => setSelectedPoint(null)}>
-              <div>{`Meter #${selectedPoint.number}`}</div>
-            </InfoWindow>
-          )}
         </Map>
       </APIProvider>
     </div>
